2f65231f2e0b5659d6babcde254bbc9e38b9ea7a
aakash <aakash.chandhoke24@gmail.com>
2017-05-20 11:20:38 +0530

    database in mlab

--- models/db.js
+++ models/db.js
@@ -4,9 +4,9 @@ var os = require("os");
 exports.mongoConfig = {
     /*serverUrl: "mongodb://localhost:27017/",
     database: "url"*/
-    /*serverUrl:"mongodb://heroku_qz1zxj4q:8m1heu07sf2790k6vk540nri59@ds011291.mlab.com:11291/",
-    database: "heroku_qz1zxj4q"*/
-    url:process.env.MONGOLAB_URI;        
+    serverUrl:"mongodb://heroku_qz1zxj4q:8m1heu07sf2790k6vk540nri59@ds011291.mlab.com:11291/",
+    database: "heroku_qz1zxj4q"
+    /*url:process.env.MONGOLAB_URI;*/        
 };
 
 exports.webhost = "http://" + os.hostname();
