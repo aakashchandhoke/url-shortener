cdd5fca2b09377c2399295459999d6bbd1504ad8
aakash <aakash.chandhoke24@gmail.com>
2017-05-20 17:29:24 +0530

    Heroku push

--- models/db.js
+++ models/db.js
@@ -2,10 +2,10 @@ var exports = module.exports = {};
 var os = require("os");
 
 exports.mongoConfig = {
-    /*serverUrl: "mongodb://localhost:27017/",
-    database: "url"*/
-    serverUrl:"mongodb://heroku_qz1zxj4q:8m1heu07sf2790k6vk540nri59@ds011291.mlab.com:11291/",
-    database: "heroku_qz1zxj4q"
+    serverUrl: "mongodb://localhost:27017/",
+    database: "url"
+    /*serverUrl:"mongodb://heroku_qz1zxj4q:8m1heu07sf2790k6vk540nri59@ds011291.mlab.com:11291/",
+    database: "heroku_qz1zxj4q"*/
     /*url:process.env.MONGOLAB_URI;*/        
 };
 
